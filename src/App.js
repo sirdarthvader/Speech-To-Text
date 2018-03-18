d9e6364b3d865c694c3d1c517b538e58b9f015cd
Ashish Nandan Singh <ashishcodes4@gmail.com>
2018-03-19 04:33:13 +0530

    adding new files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Searchbar from './Components/Searchbar/Searchbar';
+
+const API_KEY = 'AIzaSyDgLXWOnihs40bAqMvT2NmETGHHiHC4DrM';
+
 
 class App extends Component {
   render() {
@@ -10,9 +14,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Searchbar />
       </div>
     );
   }
