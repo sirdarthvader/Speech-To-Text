70c7146540657a7798eb89d065916bc5d8d0587a
Ashish Nandan Singh <ashishcodes4@gmail.com>
2018-03-19 14:18:22 +0530

    adding searchbar.js

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Searchbar = () => {
-    return <input />;
+class Searchbar extends Component {
+  render() {
+    return <input onChange={this.onInputChange} />;
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
 }
 
 export default Searchbar;
